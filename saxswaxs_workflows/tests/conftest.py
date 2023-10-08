985b108fbc142adffa3a37fc2e407630938bd166
Wiebke Köpp <wiebke.koepp@googlemail.com>
2023-10-08 15:24:24 -0700

    Add Prefect testing fixture

--- saxswaxs_workflows/tests/conftest.py
+++ saxswaxs_workflows/tests/conftest.py
@@ -1,2 +1,10 @@
-# import pytest
+import pytest
+from prefect.testing.utilities import prefect_test_harness
+
 # code for fixtures (datasets, configs, ..)
+
+
+@pytest.fixture(autouse=True, scope="session")
+def prefect_test_fixture():
+    with prefect_test_harness():
+        yield
